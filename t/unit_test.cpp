becc647a116d1d02cdafe00fe57060301ced64ce
Pavel I. Kryukov <kryukov@frtk.ru>
2018-07-08 01:16:11 +0300

    Use CATCH2 instead of googletest

--- t/unit_test.cpp
+++ t/unit_test.cpp
@@ -1,121 +1,115 @@
 #include <cstring>
 
-// Google test
-#include <gtest/gtest.h>
+// CATCH2
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
 // uArchSim modules
 #include "../kryucow_string.h"
 
-TEST( Cow_String, Equality)
+TEST_CASE( "Equality", "Cow_String")
 {
     const KryuCowString a ("Something");
     std::string a_s = "Something_strange";
-    ASSERT_NE( a, a_s);
+    REQUIRE( a != a_s);
     a_s.erase( a_s.find('_'));
-    ASSERT_EQ( a, a_s); 
+    REQUIRE( a == a_s); 
 
     const std::string_view a_v = "Something_else";
-    ASSERT_NE( a, a_v);
+    REQUIRE( a != a_v);
     const auto a_v1 = a_v.substr(0, 9);
-    ASSERT_EQ( a, a_v1);
+    REQUIRE( a == a_v1);
 }
 
-TEST( Cow_String, Copy_Check)
+TEST_CASE( "Copy_Check", "Cow_String")
 {
     KryuCowString a {"Hello"};
     KryuCowString b = a;
-    ASSERT_EQ( a, b);
-    ASSERT_EQ( a.data(), b.data());
+    REQUIRE( a == b);
+    REQUIRE( a.data() == b.data());
 
     b += " World!";
-    ASSERT_EQ( a, "Hello");
-    ASSERT_NE( b, "Hello");
-    ASSERT_EQ( b, "Hello World!");
-    ASSERT_NE( a, b);
-    ASSERT_NE( a.data(), b.data());
+    REQUIRE( a == "Hello");
+    REQUIRE( b != "Hello");
+    REQUIRE( b == "Hello World!");
+    REQUIRE( a != b);
+    REQUIRE( a.data() != b.data());
 }
 
-TEST( Cow_String, Assignment_Check)
+TEST_CASE( "Assignment_Check", "Cow_String")
 {
     KryuCowString a {"Hello"};
     const KryuCowString b = a;
-    ASSERT_EQ( a, b);
-    ASSERT_EQ( a.data(), b.data());
+    REQUIRE( a == b);
+    REQUIRE( a.data() == b.data());
 
     a = "Goodbye!";
-    ASSERT_EQ( a, "Goodbye!");
-    ASSERT_EQ( b, "Hello");
-    ASSERT_NE( a, b);
-    ASSERT_NE( a.data(), b.data());
+    REQUIRE( a == "Goodbye!");
+    REQUIRE( b == "Hello");
+    REQUIRE( a != b);
+    REQUIRE( a.data() != b.data());
 }
 
-TEST( Cow_String, Empty_String)
+TEST_CASE( "Empty_String", "Cow_String")
 {
     KryuCowString a;
-    ASSERT_EQ( a, "");
-    ASSERT_TRUE( a.empty());
+    REQUIRE( a == "");
+    REQUIRE( a.empty() );
     KryuCowString b = a;
-    ASSERT_EQ( b, "");
-    ASSERT_TRUE( b.empty());
+    REQUIRE( b == "");
+    REQUIRE( b.empty() );
 
     a = "Hello!";
-    ASSERT_EQ( a, "Hello!");
-    ASSERT_FALSE( a.empty());
-    ASSERT_EQ( b, "");
-    ASSERT_TRUE( b.empty());
-    ASSERT_NE( a, b);
+    REQUIRE( a == "Hello!");
+    REQUIRE( !a.empty() );
+    REQUIRE( b == "");
+    REQUIRE( b.empty() );
+    REQUIRE( a != b);
 
     a = "";
-    ASSERT_EQ( a, "");
-    ASSERT_TRUE( a.empty());
+    REQUIRE( a == "");
+    REQUIRE( a.empty() );
 
     b = "Goodbye!";
-    ASSERT_EQ( b, "Goodbye!");
-    ASSERT_NE( a, b);
+    REQUIRE( b == "Goodbye!");
+    REQUIRE( a != b);
 
     KryuCowString c;
     const KryuCowString d = c;
-    ASSERT_EQ( c, "");
-    ASSERT_TRUE( c.empty());
+    REQUIRE( c == "");
+    REQUIRE( c.empty() );
     c += "World!";
-    ASSERT_EQ( c, "World!");
-    ASSERT_EQ( d, "");
+    REQUIRE( c == "World!");
+    REQUIRE( d == "");
 }
 
-TEST( Wide_Cow_String, Initializing_Checks)
+TEST_CASE( "Initializing_Checks", "Wide_Cow_String")
 {
     WKryuCowString a;
     const WKryuCowString b = a;
-    ASSERT_EQ( a, b);
+    REQUIRE( a == b);
 
     WKryuCowString c( L"Hello!");
-    ASSERT_EQ( c, L"Hello!");
+    REQUIRE( c == L"Hello!");
 
     a += c;
-    ASSERT_EQ( a, c);
-    ASSERT_NE( a, b);
-    ASSERT_EQ( a, L"Hello!");
+    REQUIRE( a == c);
+    REQUIRE( a != b);
+    REQUIRE( a == L"Hello!");
 
     c += a;
-    ASSERT_EQ( c, L"Hello!Hello!");
-    ASSERT_EQ( a, L"Hello!");
+    REQUIRE( c == L"Hello!Hello!");
+    REQUIRE( a == L"Hello!");
 }
 
-TEST( Cow_String, At)
+TEST_CASE( "At", "Cow_String")
 {
     const KryuCowString a("Hello Goodbye!");
-    ASSERT_EQ( a.length(), std::strlen("Hello Goodbye!"));
-    ASSERT_EQ( a[0], 'H');
-    ASSERT_EQ( a.front(), 'H');
-    ASSERT_EQ( a.back(), '!');
-    ASSERT_EQ( a.at(3), 'l');
-    ASSERT_EQ( a[4], 'o');
-}
-
-int main( int argc, char* argv[])
-{
-    ::testing::InitGoogleTest( &argc, argv);
-    ::testing::FLAGS_gtest_death_test_style = "threadsafe";
-    return RUN_ALL_TESTS();
+    REQUIRE( a.length() == std::strlen("Hello Goodbye!"));
+    REQUIRE( a[0] == 'H');
+    REQUIRE( a.front() == 'H');
+    REQUIRE( a.back() == '!');
+    REQUIRE( a.at(3) == 'l');
+    REQUIRE( a[4] == 'o');
 }
 
